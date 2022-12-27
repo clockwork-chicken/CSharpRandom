ebfbe5c84da34f19b7166c8eaf993a70bb006914
Aryan Soni <aryan.soni@outlook.com>
2022-12-27 18:01:33 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Csharp_learning_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            for (int x = 0; x < 10000 ; x += 1)
+            {
+                int y = new Random().Next(1, 50);
+                int z = y * 2 + 1;
+                if (z > 100)
+                {
+                    Console.WriteLine("Containment Breach");
+                    break;
+                }
+                    
+                Console.WriteLine(z);
+
+            }
+            // Console.WriteLine("The total is " + total);
+        }
+    }
+}
